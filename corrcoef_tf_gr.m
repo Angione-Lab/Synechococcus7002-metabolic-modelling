b57d701ab06dcf636b520d0e9d3659bdeeb3790d
Supreeta Vijayakumar <supreetabv@yahoo.co.uk>
2020-08-27 13:13:17 +0100

    Add files via upload

---
+++ corrcoef_tf_gr.m
@@ -0,0 +1,25 @@
+%This script calculates Pearson correlation coefficients 
+%between a subset of the transcript/flux FC dataset and
+%vector of growth rates across 12 conditions
+
+%Create output vectors to store correlation coefficients, p-values, and lower and upper bounds of confidence
+%intervals, changing no. of rows for transcripts(3187), fluxes (742), or both (3929)
+corr=zeros(3187,1); % PCC
+pval=zeros(3187,1); % p-value
+lb95=zeros(3187,1); % lower bound for 95% confidence
+ub95=zeros(3187,1); % lower bound for 95% confidence
+
+ N = size(transcripts,2); % Specify size and type of data matrix: transcripts/all_atp_flux/all_p1_flux,all_p2_flux
+ for i=1:N
+     [R,P,RL,RU] = corrcoef(transcripts(:,i),Y2); %Y2 contains growth rates
+     corr(i) = R(1,2);
+     pval(i) = P(1,2);
+     lb95(i) = RL(1,2);
+     ub95(i) = RU(1,2);
+ end
+  
+% N = size(absolute_P2TF_FC,2); % Specify size of data matrix
+% for i=1:N
+%    tmp = corr(absolute_P2TF_FC(:,i),Y2,'Type','Pearson'); %Y1 contains doubling %, Y2 contains growth rates
+%    D(i,:)=tmp;
+% end
